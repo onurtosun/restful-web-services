5d8a82f51ad6ae6b169922d74a33b8a73d21a72b
ONUR TOSUN <onur_tosun@windowslive.com>
2021-02-15 14:35:10 +0300

    Restfull Web Services Çalışmaları

--- src/main/java/com/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,6 +2,13 @@ package com.webservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -9,5 +16,10 @@ public class RestfulWebServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
-
+	@Bean
+	public LocaleResolver localeResolver(){
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
 }
--- src/main/java/com/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,9 +1,17 @@
 package com.webservices.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path ="/hello-world")
     public String helloWorld(){
@@ -18,6 +26,10 @@ public class HelloWorldController {
     @GetMapping(path ="/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello World, %s",name),"Title");
+    }
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        return messageSource.getMessage("good.morning.message",null, LocaleContextHolder.getLocale());
     }
 
 }
